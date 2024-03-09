e8385c5e775bfe9574fa5ee988aeae2b1ce269d3
Or-If <143971808+Or-If@users.noreply.github.com>
2024-03-08 18:24:37 -0800

    adding user lists

--- src/js/home.js
+++ src/js/home.js
@@ -7,29 +7,44 @@ import 'toastr/toastr.scss';
 
 class Bookshelf {
   constructor() {
-    // these are used for pulling up userInfo
+    //                                                                         delete these and implement into code better
     this.allData; // I can prob change this one to scoped variable, will mess with later..... possible with other 2 as well
     this.userID;  
     this.userInfo;
 
-    // I want there to be 3 arrays that we will add books into depending on users wants
-    this.UIBooksRead = []
-    this.UIBooksReading = []
-    this.UIBooksToRead = []
-
+    
+    //                                                                         Add these all to a single function
     this.accountForm = document.querySelector('#accountSearch')
     this.accountForm.onsubmit = this.searchAccount.bind(this)
 
     this.creatNewAccountForm = document.querySelector('#accountCreation')
     this.creatNewAccountForm.onsubmit = this.addNewUser.bind(this)
 
+    document.querySelector("#addBookRead").onclick = this.addBookToList.bind(this, 'booksRead');
+    document.querySelector("#addBookReading").onclick = this.addBookToList.bind(this, 'booksReading');
+    document.querySelector("#addBookToRead").onclick = this.addBookToList.bind(this, 'booksToRead');
+
+    //this.changeButton = document.querySelector(".changeSubmit") // this wasnt working for some reason
+    this.changeButton = document.querySelector("#changeSubmit")
+    this.changeButton.onclick = this.changeFormButton.bind(this);
+
     // I might use a few more document slectors to add events onto page, but not sure atm
     this.UIBookSearch = document.querySelector('#bookSearch')
     this.UIBookSearch.onsubmit = this.searchBook.bind(this)
+
+
     this.bookOnDisplay = {
       title: "Anciallary Justice",
       author: "Ann Leckie",
       oclc: "828142663",
+    }
+
+    this.currentUser = {
+      id: NaN,
+      name: "",
+      booksRead: [],
+      booksReading: [],
+      booksToRead: []
     }
   }
 
@@ -93,8 +108,16 @@ class Bookshelf {
   }
 
   setAccountToPage() {
-    document.querySelector("#accountInfo").innerHTML = 
-      `<p>Hello ${this.userInfo.name} I hope you are enjoying my website!`
+    document.querySelector("#accountName").innerHTML = 
+      `${this.userInfo.name}'s <b>BookShelf</b>`
+    this.currentUser.id = this.userInfo.id
+    this.currentUser.name = this.userInfo.name
+    this.currentUser.booksRead = this.userInfo.booksRead
+    this.currentUser.booksReading = this.userInfo.booksReading
+    this.currentUser.booksToRead = this.userInfo.booksToRead
+    this.updatePageLists("booksToRead")
+    this.updatePageLists("booksRead")
+    this.updatePageLists("booksReading")
   }
   //                                                                           This would encompass all script related to finding user
  
@@ -107,7 +130,7 @@ class Bookshelf {
   async addNewUser(event) {
     event.preventDefault();
     this.addUserToAuthentication(usernameCreation.value, passwordCreation.value)
-    this.addUserToUsers(usernameCreation.value)
+    this.addUserToUsersDB(usernameCreation.value)
   }
 
   async addUserToAuthentication(username, password) {
@@ -121,7 +144,7 @@ class Bookshelf {
     const response = await fetch(USER_AUTH, requestOptions)
   }
 
-  async addUserToUsers(username) {
+  async addUserToUsersDB(username) {
     const newUserInfo = new this.userAccount(username)
     const requestOptions = {
       method: 'POST',
@@ -136,6 +159,7 @@ class Bookshelf {
     this.username = username,
     this.password = password
   }
+
   userAccount(name) {
     this.name = name,
     this.booksRead = [],
@@ -144,7 +168,7 @@ class Bookshelf {
   }
 
 
-
+  //                                             //
 
 
 
@@ -167,6 +191,52 @@ class Bookshelf {
   setBookToPage() {
     document.querySelector('#displayBook').innerHTML = 
       `<p>${this.bookOnDisplay.title} by: ${this.bookOnDisplay.author}</p><img src='https://covers.openlibrary.org/b/oclc/${this.bookOnDisplay.oclc}-M.jpg'>`
+    document.querySelector('#displayButtons').classList.remove("d-none");
   }
+
+  //                                                    //
+  
+  changeFormButton() {
+    if(this.creatNewAccountForm.classList.contains('d-none')) {
+      this.creatNewAccountForm.classList.remove('d-none')
+      this.changeButton.innerHTML = "Already have an account"
+      this.accountForm.classList.add('d-none')
+    } else {
+      this.accountForm.classList.remove('d-none')
+      this.changeButton.innerHTML = "Create an account instead"
+      this.creatNewAccountForm.classList.add('d-none')
+    }
+  }
+
+  //                                                      //
+
+  addBookToList(list) {
+    console.log(this.currentUser)
+    console.log(this.bookOnDisplay)
+    console.log(list)
+    const bookToPush = {
+      title: this.bookOnDisplay.title,
+      author: this.bookOnDisplay.author,
+      oclc: this.bookOnDisplay.oclc
+    }
+    this.currentUser[list].push(bookToPush)
+    this.updatePageLists(list);
+    this.pushToDataBase(list);
+    // push on to the DB
+  }
+
+  updatePageLists(list) {
+    console.log(this.currentUser[list].length ) // this is to see if the user has any lists to add, if they dont than we wont add html of nothing
+    if(this.currentUser[list].length > 0) {
+      document.getElementById(list).innerHTML = " ";
+      for(let i=0; i < this.currentUser[list].length; i++) {
+          document.getElementById(list).innerHTML += 
+        `<p>${this.currentUser[list][i].title} <br> by: ${this.currentUser[list][i].author}</p> 
+        <img width="100px" height="150px" src='https://covers.openlibrary.org/b/oclc/${this.currentUser[list][i].oclc}-M.jpg'>`
+      }
+    }
+  }
+
+
 }
 window.onload = () => {new Bookshelf}
